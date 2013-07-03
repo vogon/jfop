bebd4136d6510709cc36e95476b9eae4cce299a7
Colin <vogon@icculus.org>
2013-07-02 22:37:23 -0700

    let's try and figure out how to use this twilio shit

--- web.rb
+++ web.rb
@@ -41,6 +41,8 @@ class JFOP < Sinatra::Base
 			# LAST_PIZZA = PizzaData.new(time: DateTime.now, msg: params['msg'])
 			# LAST_PIZZA.dump(LAST_PIZZA_FILENAME)
 
+			print request.body.read
+
 			slim :twiml
 		end
 	end
